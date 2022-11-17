660302c6a3a1f8447963ba6df87a3f8bcd5f1c77
Philip Barrison <81592117+phbarris@users.noreply.github.com>
2022-11-17 12:24:11 -0500

    11/17 - Institution summary dataframe created.

--- SPC.py
+++ SPC.py
@@ -4,33 +4,59 @@ import numpy as np
 # Load source csv file
 inputs = pd.read_csv("test.csv")
 
-# Get a list of institution IDs
+# Get lists of institution IDs, measures, and staff roles to stratify outcomes by
+## Get a list of institution IDs
 inputs = inputs.sort_values(["Institution_ID"])
 institution_list = inputs["Institution_ID"].drop_duplicates()
-
-# Get a list of measures
+## Get a list of measures
 measures = inputs["Measure"].drop_duplicates()
-
-# Get a list of staff roles
+## Get a list of staff roles
 staff_role = inputs["Staff_Type"].drop_duplicates()
 
-# Collect summary data per institution-per measure-per peer group
-institution_summary = [] # creates an empty list that will contain each institutions data
-for i in institution_list:
-    institution = inputs[inputs["Institution_ID"] == i] #separates individual institutions
-    #output.to_csv("Institution" + str(i))
 
-    #find mean, std, ucl, lcl of each institutions pass percentage
-    for j in measures:
-        measure = institution[institution["Measure"] == j] #separates measures per institution
+# Collect summary data per institution, per measure, per peer group
+def parse_institutions(inputs):
 
-        for k in staff_role:
-            staff = measure[measure["Staff_Type"] == k] # separates measures per peer group
-            mean = staff["Pass_percentage"].mean()
-            std = staff["Pass_percentage"].std()
-            lcl = -3 * std
-            ucl = 3 * std
+    list_institutions = [] #Create lists for output variables
+    list_measures = []
+    list_staffrole = []
+    list_means = []
+    list_std = []
+    list_lcl = []
+    list_ucl = []
 
-            institution_summary.append([i, j, k, mean, std, lcl, ucl])
+    for i in institution_list:
+        institution = inputs[inputs["Institution_ID"] == i] #separates individual institutions
+        for j in measures:
+            measure = institution[institution["Measure"] == j] #separates measures per institution
+            for k in staff_role:
+                staff = measure[measure["Staff_Type"] == k] # separates measures per peer group
+                mean = staff["Pass_percentage"].mean()
+                std = staff["Pass_percentage"].std()
+                lcl = mean - (3 * std)
+                ucl = mean + (3 * std)
 
-print(institution_summary)
+                list_institutions.append(i) #Add values to lists
+                list_measures.append(j)
+                list_staffrole.append(k)
+                list_means.append(mean)
+                list_std.append(std)
+                list_lcl.append(lcl)
+                list_ucl.append(ucl)
+
+                institution_d = { #transform lists into a dictionary for dataframe conversion
+                    "Institution": list_institutions,
+                    "Measure": list_measures,
+                    "Staff_Role": list_staffrole,
+                    "Mean": list_means,
+                    "Standard_Deviation": list_std,
+                    "Lower_Control_Limit": list_lcl,
+                    "Upper_Control_Limit": list_ucl
+                }
+
+    institution_df = pd.DataFrame(data= institution_d)
+    
+    return(institution_df)
+
+print(parse_institutions(inputs))
+parse_institutions(inputs).to_csv("Institution_Summary.csv")
