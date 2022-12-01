a0dedd1df2c7f657d320e064377eec6678db227c
Philip Barrison <81592117+phbarris@users.noreply.github.com>
2022-12-01 00:37:53 -0500

    Update SPC for Johnson transformation

--- SPC.py
+++ SPC.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import shapiro
+from sklearn.preprocessing import PowerTransformer
 
 # Load source csv file
 inputs = pd.read_csv("Raw_Data.csv")
@@ -45,6 +46,26 @@ def parse_institutions(inputs):
                 distribution = False
                 if p > alpha:
                     distribution = True
+
+                if distribution == False:
+                    yeojohnTr = PowerTransformer(standardize=True)
+                    df_yeojohn = pd.DataFrame(yeojohnTr.fit_transform(staff_role["Pass_percentage"].values.reshape(-1,1))) ##Apply Johnson Transformation to skewed data
+                    transformed_staff_role = staff_role.reset_index()
+                    transformed_staff_role["Transformed"] = df_yeojohn[0]
+                
+                    #plt.figure(figsize=(15,6))
+                    #plt.title(str(i) + str(j) + str(k), fontsize=15)
+                    #sns.histplot(transformed_staff_role["Transformed"], kde=True, color="red")
+                    #plt.show()
+
+                    stat, p = shapiro(transformed_staff_role["Transformed"]) #Asses for presence of normal distribution
+                    if p > alpha:
+                        distribution = True #If true, update institutional data for SPC
+                        mean = transformed_staff_role["Transformed"].mean()
+                        std = transformed_staff_role["Transformed"].std()
+                        lcl = mean - (3 * std)
+                        ucl = mean + (3 * std)  
+
 
                 #Add values to lists
                 list_institutions.append(i)
